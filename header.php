1855dce3af0c2ec163e328ae429b0bf49b2f6790
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2022-05-23 02:30:49 -0300

    ordenando

---
+++ header.php
@@ -0,0 +1,10 @@
+	<div class="header gradient">
+      <img width="4%" src="imagenes/latex-macros-packages.png">
+      
+      <div class="top-nav darkblock">
+        <a href="http://latex-macros-packages.sf.net/">Home</a> |
+        <a href="http://sourceforge.net/projects/latex-macros-packages/">Develop</a> |
+        <a href="https://sourceforge.net/p/latex-macros-packages/_list/git">Download</a> |                
+        <a href="https://sourceforge.net/p/latex-macros-packages/_members/">Contact</a>
+      </div>
+	</div>
--- index.php
+++ index.php
@@ -1,89 +1,131 @@
 <html>
 <head>
 	<title> ..:: LATEX-MACROS-PACKAGES ::..</title>
-	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+	<meta http-equiv="content-type" content="text/html; charset=UTF8">
   	<link href="imagenes/base.css" media="screen" rel="stylesheet" type="text/css">
 	 <meta http-equiv="X-UA-Compatible" content="chrome=1">
 </head>
 <body>
 
-<?php
- ini_set('default_charset','ISO-8859-1');
-?>
+	<?php
+	 ini_set('default_charset','UTF8');
+	?>
+
+	<?php 
+	include 'header.php';
+	?>
 
 	<div class="header gradient">
-      <div class="top-nav darkblock">
-        <a href="http://latex-macros-packages.sf.net/">Home</a> |
-        <a href="http://sourceforge.net/projects/latex-macros-packages/">Develop</a> |
-        <a href="http://sourceforge.net/projects/latex-macros-packages/files/">Download</a> |                
-        <a href="https://sourceforge.net/p/latex-macros-packages/_members/">Contact</a>
-      </div>
-
-      <img width="4%" src="imagenes/latex-macros-packages.png">
-
-      <div class="promo-area">
-
-        <img style="float: left;" width="50%" src="imagenes/latex-macros-packages-screenshot.png">
-        
-        <h2>LATEX-MACROS-PACKAGES</h2>
-        <p>Varios macros en latex.
-		</p>
+	<div class="promo-area">
+		<h2>LATEX-MACROS-PACKAGES</h2>
 		<p>
-			Desde la pagina des descargas de
-			<a href="https://sourceforge.net/projects/latex-macros-packages/files/"><b>latex-macros-packages</b></a> 
-			pueden ser obtenidos los paquetes.
+			Podemos descargar vários paquetes de macros desde los repositorios git 
+			<a href="https://sourceforge.net/p/latex-macros-packages/_list/git"><b>latex-macros-packages</b></a> 
 		</p>        
-      </div>
-      
+	</div>
 	</div>
 
-	<div class="content">
+		
+	<b><h2>Macros:</h2></b>
+	<div class="header libgradient">
+	
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a target="_blank" href="https://sourceforge.net/p/latex-macros-packages/math-macros/ci/main/tree/">
+		<div class="imagedemo">
+		<img width="125px" height="125px" src="imagenes/latex-macros-packages.png"/>
+		</div>
+		<b>Math Macros</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a target="_blank" href="https://sourceforge.net/p/latex-macros-packages/catalographic-card/ci/main/tree/">
+		<div class="imagedemo">
+		<img width="125px" src="https://sourceforge.net/p/latex-macros-packages/catalographic-card/ci/main/tree/screenshot.png?format=raw"/>
+		</div>
+		<b>Catalographic Card</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a target="_blank" href="https://sourceforge.net/p/latex-macros-packages/separator-rule/ci/main/tree/">
+		<div class="imagedemo">
+		<img width="125px" src="https://sourceforge.net/p/latex-macros-packages/separator-rule/ci/main/tree/screenshot.png?format=raw"/>
+		</div>
+		<b>Separator Rule</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a href="https://sourceforge.net/p/latex-macros-packages/_list/git">
+		<div class="imagedemo">
+		<img width="125px" src="imagenes/latex-macros-packages.png"/>
+		</div>
+		<b>Theorem</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a href="https://sourceforge.net/p/latex-macros-packages/_list/git">
+		<div class="imagedemo">
+		<img width="125px" src="imagenes/latex-macros-packages.png"/>
+		</div>
+		<b>Enumerate</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a href="https://sourceforge.net/p/latex-macros-packages/_list/git">
+		<div class="imagedemo">
+		<img width="125px" src="imagenes/latex-macros-packages.png"/>
+		</div>
+		<b>Item</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a href="https://sourceforge.net/p/latex-macros-packages/_list/git">
+		<div class="imagedemo">
+		<img width="125px" src="imagenes/latex-macros-packages.png"/>
+		</div>
+		<b>Equation Box</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a href="https://sourceforge.net/p/latex-macros-packages/_list/git">
+		<div class="imagedemo">
+		<img width="125px" src="imagenes/latex-macros-packages.png"/>
+		</div>
+		<b>Citation Box</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a href="https://sourceforge.net/p/latex-macros-packages/_list/git">
+		<div class="imagedemo">
+		<img width="125px" src="imagenes/latex-macros-packages.png"/>
+		</div>
+		<b>Elaboration Box</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->	
+	<div class="whiteblock libblock"><a href="https://sourceforge.net/p/latex-macros-packages/_list/git">
+		<div class="imagedemo">
+		<img width="125px" src="imagenes/latex-macros-packages.png"/>
+		</div>
+		<b>Information Box</b>
+			texto texto      
+		</a>   
+	</div>
+	<!-- *********************************************************************** -->
+	
+	</div>
 
-      <div class="box">
-		<h3>Que es el software libre?</h3>
-        <div class="box-content">
-<iframe width="180" height="150" src="https://www.youtube.com/embed/FvLJ2JotttM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>       
-      </div>
-      
-      <div class="box">
-        <h3>Social</h3>
-        <div class="box-content">
-          <ul class="services">
-            <li><a href="https://gnusocial.net/trucomanx"><img src="imagenes/gnusocial.png">GnuSocial</a></li>
-          </ul>
-          <ul class="services">
-            <li><a href="https://diasp.org/people/68e40bb7f3f6d792"><img src="imagenes/diaspora+logo.png">Diaspora</a></li>
-          </ul>
-        </div>
-        <h3>Sourceforge</h3>
-        <div class="box-content">
-        <a href="https://sourceforge.net/projects/latex-macros-packages/files/latest/download"><img alt="Download latex-macros-packages" src="https://img.shields.io/sourceforge/dm/latex-macros-packages.svg" ></a>
-        </div>
-      </div>      
-       
-      <div class="box">
-        <h3>News</h3>
-        <div class="box-content">
-<?php
-$rss_feed = simplexml_load_file("https://sourceforge.net/p/latex-macros-packages/activity/feed");
-if(!empty($rss_feed)) 
-{
-    $i=0;
-    foreach ($rss_feed->channel->item as $feed_item) 
-    {
-        if($i>=4) break;
-        ?>
-        <div><a class="feed_title" href="<?php echo $feed_item->link; ?>"><b><?php echo $feed_item->title; ?></b></a></div>
-        <div><?php echo implode(' ', array_slice(explode(' ', $feed_item->description), 0, 14)) . "..."; ?></div>
-        <?php		
-        $i++;	
-    }
-}
-?>
-        </div>
-      </div>
+	<?php 
+	include 'footer.php';
+	?>
 
-    </div>
-
-  </body></html>
+</body>
+</html>
