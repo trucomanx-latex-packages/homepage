bf3d5652ccdc952ffe516b3209c49bd192f659a2
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2022-05-27 00:51:32 -0300

    Agregando

--- env-enumerate/env-enumerate.php
+++ env-enumerate/env-enumerate.php
@@ -51,6 +51,19 @@ include '../include/header-sub.php';
 	</p>
 </div>
 <!-- *********************************************************************** -->
+<div class="header libgradient">
+	<h2>env-bulletjournalribbon</h2>
+	<p>
+		Download the latex package of env-bulletjournalribbon from
+		<a href="https://sourceforge.net/p/latex-macros-packages/env-bulletjournalribbon/ci/master/tree">
+		this link:<br>
+		<div class="whiteblock">
+		<img src="https://sourceforge.net/p/latex-macros-packages/env-bulletjournalribbon/ci/master/tree/screenshot.png?format=raw"/>
+		</div>
+		</a> 
+	</p>
+</div>
+<!-- *********************************************************************** -->
 
 <?php 
 include '../include/footer-sub.php';
