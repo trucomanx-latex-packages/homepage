ce5c4cbabf3f3a6b0437b4381ca179daf57843d1
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-08-31 19:44:56 -0300

    ordenando

--- env-theorem/env-theorem.php
+++ env-theorem/env-theorem.php
@@ -51,6 +51,19 @@ include '../include/header-sub.php';
 	</p>
 </div>
 <!-- *********************************************************************** -->
+<div class="header libgradient">
+	<h2>env-theorem-simpleimg</h2>
+	<p>
+		Download the latex package of env-theorem-simpleimg from
+		<a href="https://sourceforge.net/p/latex-macros-packages/env-theorem-simpleimg/ci/master/tree/">
+		this link:<br>
+		<div class="whiteblock">
+		<img src="https://sourceforge.net/p/latex-macros-packages/env-theorem-simpleimg/ci/master/tree/screenshot.png?format=raw"/>
+		</div>
+		</a> 
+	</p>
+</div>
+<!-- *********************************************************************** -->
 
 <?php 
 include '../include/footer-sub.php';
