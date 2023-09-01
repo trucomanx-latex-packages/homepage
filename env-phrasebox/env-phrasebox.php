fb195e0ec34f46c54ea614dbadfe6af9e90bfeed
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-08-31 22:55:45 -0300

    ordenando

--- env-phrasebox/env-phrasebox.php
+++ env-phrasebox/env-phrasebox.php
@@ -25,6 +25,19 @@ include '../include/header-sub.php';
 	</p>
 </div>
 <!-- *********************************************************************** -->
+<div class="header libgradient">
+	<h2>env-phrasebox-ripple</h2>
+	<p>
+		Download the latex package of env-phrasebox-ripple from
+		<a href="https://sourceforge.net/p/latex-macros-packages/env-phrasebox-ripple/ci/main/tree/">
+		this link:<br>
+		<div class="whiteblock">
+		<img src="https://sourceforge.net/p/latex-macros-packages/env-phrasebox-ripple/ci/master/tree/screenshot.png?format=raw"/>
+		</div>
+		</a> 
+	</p>
+</div>
+<!-- *********************************************************************** -->
 
 <?php 
 include '../include/footer-sub.php';
