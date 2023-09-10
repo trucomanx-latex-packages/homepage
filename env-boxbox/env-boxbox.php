ac122a469ac9f4ae635130c5f87b100ded557924
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-09-09 22:03:25 -0300

    ordenando

---
+++ env-boxbox/env-boxbox.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+	<?php 
+	include '../include/head-sub.php';
+	?>
+</head>
+<body>
+
+<?php
+ini_set('default_charset','UTF8');
+include '../include/header-sub.php';
+?>
+
+<!-- *********************************************************************** -->
+<div class="header libgradient">
+	<h2>env-box-simple</h2>
+	<p>
+		Download the latex package of env-box-simple from
+		<a href="https://sourceforge.net/p/latex-macros-packages/env-box-simple/ci/master/tree/">
+		this link:<br>
+		<div class="whiteblock">
+		<img src="https://sourceforge.net/p/latex-macros-packages/env-box-simple/ci/master/tree/screenshot.png?format=raw"/>
+		</div>
+		</a> 
+	</p>
+</div>
+<!-- *********************************************************************** -->
+<div class="header libgradient">
+	<h2>env-box-pictbox</h2>
+	<p>
+		Download the latex package of env-box-pictbox from
+		<a href="https://sourceforge.net/p/latex-macros-packages/env-box-pictbox/ci/master/tree/">
+		this link:<br>
+		<div class="whiteblock">
+		<img src="https://sourceforge.net/p/latex-macros-packages/env-box-pictbox/ci/master/tree/screenshot.png?format=raw"/>
+		</div>
+		</a> 
+	</p>
+</div>
+<!-- *********************************************************************** -->
+<div class="header libgradient">
+	<h2>env-box-oddtab</h2>
+	<p>
+		Download the latex package of env-box-oddtab from
+		<a href="https://sourceforge.net/p/latex-macros-packages/env-box-oddtab/ci/master/tree/">
+		this link:<br>
+		<div class="whiteblock">
+		<img src="https://sourceforge.net/p/latex-macros-packages/env-box-oddtab/ci/master/tree/screenshot.png?format=raw"/>
+		</div>
+		</a> 
+	</p>
+</div>
+<!-- *********************************************************************** -->
+
+<?php 
+include '../include/footer-sub.php';
+?>
+
+</body>
+</html>
