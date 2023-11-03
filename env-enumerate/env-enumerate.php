c9429a7869d6e0b104cb1240c09ad2cf15bf446f
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-11-03 03:19:33 -0300

    ordenando

--- env-enumerate/env-enumerate.php
+++ env-enumerate/env-enumerate.php
@@ -11,19 +11,7 @@ ini_set('default_charset','UTF8');
 include '../include/header-sub.php';
 ?>
 
-<!-- *********************************************************************** -->
-<div class="header libgradient">
-	<h2>env-bulletjournalround</h2>
-	<p>
-		Download the latex package of env-bulletjournalround from
-		<a href="https://sourceforge.net/p/latex-macros-packages/env-bulletjournalround/ci/main/tree/">
-		this link:<br>
-		<div class="whiteblock">
-		<img src="https://sourceforge.net/p/latex-macros-packages/env-bulletjournalround/ci/main/tree/screenshot.png?format=raw"/>
-		</div>
-		</a> 
-	</p>
-</div>
+
 <!-- *********************************************************************** -->
 <div class="header libgradient">
 	<h2>env-bulletjournalpicture</h2>
