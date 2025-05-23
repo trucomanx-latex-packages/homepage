93383c427e7845e1cd88738e0b5046869cff46d6
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-05-23 14:30:35 -0300

    ordenando

--- include/header.php
+++ include/header.php
@@ -5,7 +5,7 @@
         <a href="http://latex-macros-packages.sf.net/">Home</a> |
         <a href="http://latex-macros-packages.sf.net/examples/examples.php">Examples</a> |
         <a href="http://sourceforge.net/projects/latex-macros-packages/">Develop</a> |
-        <a href="https://sourceforge.net/p/latex-macros-packages/_list/git">Download</a> |                
+        <a href="https://sourceforge.net/p/latex-macros-packages/_list/git">Git</a> |                
         <a href="https://sourceforge.net/p/latex-macros-packages/_members/">Contact</a>
       </div>
 	</div>
